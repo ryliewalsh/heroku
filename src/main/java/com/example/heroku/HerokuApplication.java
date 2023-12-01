8d06bb80e1790107d6d304ac51de75c562bba3a1
ryliewalsh <62161188+ryliewalsh@users.noreply.github.com>
2023-12-01 11:16:54 -0700

    Update HerokuApplication.java

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -64,7 +64,7 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick") + rs.getString("random_string"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rs.getString("random_string"));
       }
 
       model.put("records", output);
