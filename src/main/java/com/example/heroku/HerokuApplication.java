251054b3679d11f3028dcb0ff9cf5b678e8e2a8c
ryliewalsh <62161188+ryliewalsh@users.noreply.github.com>
2023-11-30 22:14:32 -0700

    Update HerokuApplication.java try

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -59,11 +59,11 @@ public class HerokuApplication {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
 stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString(10) + "')");
-      ResultSet rs = stmt.executeQuery("SELECT random_string FROM table_timestamp_and_random_string");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("timestamp"));
+        output.add("Read from DB: " + rs.getTimestamp("timestamp") + rs.getRandomString("random_string");
       }
 
       model.put("records", output);
